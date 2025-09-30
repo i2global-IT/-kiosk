cb352e8e148bb2031a4126caa47ae7ef0fc6a09f
jeevitha-i2global <jeevitha@i2global.io>
2025-09-30 17:26:39 +0530

    correction

--- src/service/endpoint.tsx
+++ src/service/endpoint.tsx
@@ -1,6 +1,6 @@
 export const baseUrl = {
-baseUrl: 'https://devhrms.i2global.co.in/api/v2/',
-//  baseUrl: 'https://api.pplsync.io/api/v2/',
+//baseUrl: 'https://devhrms.i2global.co.in/api/v2/',
+ baseUrl: 'https://api.pplsync.io/api/v2/',
 pl:"https://play.google.com/store/apps/details",
   geoKey: 'AIzaSyA3yj-VGdg0hUIoUCjPaMHMjQKJSP2vkNU', 
   geoLoc: 'https://maps.googleapis.com/maps/api/geocode/json',
